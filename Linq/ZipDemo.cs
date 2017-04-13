fd4cf4dd49e2ee6bf56cf076a4488fac19423021
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2017-04-12 22:10:33 -0700

    added csharp tips

---
+++ Linq/ZipDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTips
+{
+    /// <summary>
+    /// Demonstrates how to merge Ienumerable Sequences with LINQ
+    /// </summary>
+    public class ZipDemo : IRunableProgram
+    {
+        /*  
+         The Zip method allows the joining together of IEnumerable sequences by interleaving the
+         elements in the sequences.
+         Zip is an extension method on IEnumerable.
+         */
+
+        public void Run()
+        {
+            var names = new[] { "David", "Lisa", "Amanda" };
+
+            var ages = new[] { 23, 34, 65 };
+            
+            //Generates an IEnumerable of String
+            var namesandAges = names.Zip(ages, (name, age) => name + " " + age);
+            foreach (var item in namesandAges)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Generates an IEnuerable of two element Tuples
+            var namesandAgesTuple = names.Zip(ages, (name, age) => Tuple.Create(name,age));
+            foreach (var item in namesandAgesTuple)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRunableProgram program = new ZipDemo();
+            program.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
